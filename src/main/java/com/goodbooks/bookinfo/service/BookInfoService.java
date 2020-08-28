df7938895e715c30a32f7119b4eb0689b5c44363
Indrajeet Kalra <Inder@Indrajeets-MacBook-Pro.local>
2020-08-28 14:31:39 +1000

    Removed unused code in service

--- src/main/java/com/goodbooks/bookinfo/service/BookInfoService.java
+++ src/main/java/com/goodbooks/bookinfo/service/BookInfoService.java
@@ -12,17 +12,10 @@ import java.util.Optional;
 @Service
 public class BookInfoService {
 
-/*    private List<Book> listOfBooks = Arrays.asList(
-            new Book("1", "A Suitable Boy"),
-            new Book("2", "White Tiger"),
-            new Book("3", "Freedom at midnight")
-    );*/
-
     @Autowired
     private BookRepository bookRepository;
 
     public Optional<Book> getBookById(String bookId) {
         return bookRepository.findById(bookId);
-        //return listOfBooks.stream().filter(b-> b.getBookId().equalsIgnoreCase(bookId)).findFirst();
     }
 }
